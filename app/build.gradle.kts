c28c74b73731b43978a86f979469311951012975
nouxinf <180769756+nouxinf@users.noreply.github.com>
2026-06-30 09:07:15 +0100

    Fix F-droid build, remove play libraries etc v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.nouxinf.rng"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
     }
 
